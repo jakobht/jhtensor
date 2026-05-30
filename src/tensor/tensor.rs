d4342b990cfd01eb797941cb14c9f1879e2c0581
Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>
2026-05-30 15:52:54 +0200

    Added tests for tensor
    Signed-off-by: Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>

--- src/tensor/tensor.rs
+++ src/tensor/tensor.rs
@@ -35,3 +35,43 @@ impl<B: Backend> Tensor<B> {
         B::to_vec::<T>(&self.data, num_elements)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::tensor::{CPUBackend, MetalBackend, Tensor};
+
+    mod add {
+        use super::*;
+
+        macro_rules! test_add_for {
+            ($backend:ident, $t:ident) => {
+                #[test]
+                fn $t() {
+                    let a = Tensor::<$backend>::new::<$t>(&[1 as $t, 2 as $t, 3 as $t, 4 as $t, 5 as $t], vec![5]);
+                    let b = Tensor::<$backend>::new::<$t>(&[1 as $t, 2 as $t, 3 as $t, 4 as $t, 5 as $t], vec![5]);
+
+                    let result = a.add(&b);
+                    let result_vec = result.to_vec::<$t>();
+
+                    assert_eq!(result_vec, vec![2 as $t, 4 as $t, 6 as $t, 8 as $t, 10 as $t]);
+                }
+            };
+        }
+
+        mod metal {
+            use super::*;
+
+            test_add_for!(MetalBackend, f32);
+            test_add_for!(MetalBackend, i32);
+            test_add_for!(MetalBackend, i16);
+        }
+
+        mod cpu {
+            use super::*;
+
+            test_add_for!(CPUBackend, f32);
+            test_add_for!(CPUBackend, i32);
+            test_add_for!(CPUBackend, i16);
+        }
+    }
+}
