c75c402491aeb3ac163ebd395a5eec4647de49a2
Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>
2026-05-31 15:00:21 +0200

    More tests
    Signed-off-by: Jakob Haahr Taankvist <jakob_taankvist@hotmail.com>

--- src/tensor/tensor.rs
+++ src/tensor/tensor.rs
@@ -8,6 +8,13 @@ pub struct Tensor<B: Backend> {
 
 impl<B: Backend> Tensor<B> {
     pub fn mat_mul_inplace(&self, other: &Self, dest: &mut Self) -> Result<(), TensorError> {
+        if self.shape.len() != 2 || other.shape.len() != 2 || dest.shape.len() != 2 {
+            return Err(TensorError::ShapeMismatch {
+                expected: vec![2, 2, 2],
+                got: vec![self.shape.len(), other.shape.len(), dest.shape.len()],
+            });
+        }
+
         if self.shape[1] != other.shape[0] {
             return Err(TensorError::ShapeMismatch {
                 expected: vec![self.shape[0], other.shape[1]],
@@ -152,7 +159,92 @@ mod tests {
     }
 
     mod mat_mul {
-        use crate::tensor::{CPUBackend, MetalBackend, Tensor};
+        use crate::tensor::{CPUBackend, DType, MetalBackend, Tensor, TensorError};
+
+        #[test]
+        fn test_tensor_shape_mismatch() {
+            let a = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![5]).unwrap();
+            let b = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![5, 1]).unwrap();
+            let mut dest = Tensor::<MetalBackend>::new::<f32>(&[0.0; 25], vec![5, 5]).unwrap();
+
+            let result = a.mat_mul_inplace(&b, &mut dest);
+            assert!(result.is_err());
+            assert_eq!(
+                result.err().unwrap(),
+                TensorError::ShapeMismatch {
+                    expected: vec![2,2,2],
+                    got: vec![1, 2, 2]
+                }
+            );
+        }
+
+        #[test]
+        fn test_dest_type_mismatch() {
+            let a = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![5, 1]).unwrap();
+            let b = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![1, 5]).unwrap();
+            let mut dest = Tensor::<MetalBackend>::new::<i32>(&[0; 25], vec![5, 5]).unwrap();
+
+            let result = a.mat_mul_inplace(&b, &mut dest);
+            assert!(result.is_err());
+            assert_eq!(
+                result.err().unwrap(),
+                TensorError::TypeMismatch {
+                    expected: DType::Float32,
+                    got: DType::Int32
+                }
+            );
+        }
+
+        #[test]
+        fn test_dest_shape_mismatch() {
+            let a = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![5,1]).unwrap();
+            let b = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![1,5]).unwrap();
+            let mut dest = Tensor::<MetalBackend>::new::<f32>(&[0.0; 16], vec![4, 4]).unwrap();
+
+            let result = a.mat_mul_inplace(&b, &mut dest);
+            assert!(result.is_err());
+            assert_eq!(
+                result.err().unwrap(),
+                TensorError::ShapeMismatch {
+                    expected: vec![5, 5],
+                    got: vec![4, 4]
+                }
+            );
+        }
+
+        #[test]
+        fn test_other_tensor_type_mismatch() {
+            let a = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![5, 1]).unwrap();
+            let b = Tensor::<MetalBackend>::new::<i32>(&[1, 2, 3, 4, 5], vec![1, 5]).unwrap();
+            let mut dest = Tensor::<MetalBackend>::new::<f32>(&[0.0; 25], vec![5, 5]).unwrap();
+
+            let result = a.mat_mul_inplace(&b, &mut dest);
+            assert!(result.is_err());
+            assert_eq!(
+                result.err().unwrap(),
+                TensorError::TypeMismatch {
+                    expected: DType::Float32,
+                    got: DType::Int32
+                }
+            );
+        }
+
+        #[test]
+        fn test_other_tensor_shape_mismatch() {
+            let a = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0], vec![2, 2]).unwrap();
+            let b = Tensor::<MetalBackend>::new::<f32>(&[1.0, 2.0, 3.0, 4.0, 5.0], vec![5, 1]).unwrap();
+            let mut dest = Tensor::<MetalBackend>::new::<f32>(&[0.0; 10], vec![2, 5]).unwrap();
+
+            let result = a.mat_mul_inplace(&b, &mut dest);
+            assert!(result.is_err());
+            assert_eq!(
+                result.err().unwrap(),
+                TensorError::ShapeMismatch {
+                    expected: vec![2, 1],
+                    got: vec![2, 5]
+                }
+            );
+        }
 
         macro_rules! test_fuzzy {
             ($backend:ident, $t:ident) => {
